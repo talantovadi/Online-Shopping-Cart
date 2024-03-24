2b07804475ddf516a355895f6f14547b2b569145
talantovadi <dilbartalantova@gmail.com>
2024-03-24 13:03:20 +0600

    Project base

---
+++ src/main/java/kg/talantova/shoppingcart/ShoppingCartApplication.java
@@ -0,0 +1,15 @@
+package kg.talantova.shoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ShoppingCartApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
